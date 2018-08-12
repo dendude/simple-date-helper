90e3016a0f99c82196cedf294792f531bd0edc81
Denis Kravtsov <denis.kravtsov.1986@mail.ru>
2018-08-12 21:54:32 +0300

    добавлен psr-4, расширен хелпер для работы с датой

--- src/DateHelper.php
+++ src/DateHelper.php
@@ -3,7 +3,17 @@ namespace DenDude;
 
 class DateHelper
 {
-    public static function formatDate($str, $format = 'd.m.Y H:i'): ?string
+    public static function humanDate($str): ?string
+    {
+        return self::dateFormat($str, 'd.m.Y');
+    }
+
+    public static function sqlDate($str): ?string
+    {
+        return self::dateFormat($str, 'Y-m-d');
+    }
+
+    protected static function dateFormat($str, $format): ?string
     {
         $str = strtotime($str);
         if ($str === false) return null;
